0580d2c62bcf7593edd3b695b06bdaa4529584a8
diamondpete <87245367+diamondpete@users.noreply.github.com>
2026-01-22 01:03:23 -0500

    ftp/curl: Update to 8.18.0

--- replace.sh
+++ replace.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 rm -rf /usr/ports/devel/py-cheetah3
 rm -rf /usr/ports/sysutils/webmin
+rm -rf /usr/ports/ftp/curl
 cp -Rp devel/py-cheetah3 /usr/ports/devel/py-cheetah3
 cp -Rp sysutils/webmin /usr/ports/sysutils/webmin
+cp -Rp ftp/curl /usr/ports/ftp/curl
 chown -R root:wheel /usr/ports
