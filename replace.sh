f703c8d29bef6c25c371f881424b0421cdda6df3
diamondpete <87245367+diamondpete@users.noreply.github.com>
2026-01-22 00:40:41 -0500

    sysutils/webmin: Update to 2.620

--- replace.sh
+++ replace.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
 rm -rf /usr/ports/devel/py-cheetah3
+rm -rf /usr/ports/sysutils/webmin
 cp -Rp devel/py-cheetah3 /usr/ports/devel/py-cheetah3
+cp -Rp sysutils/webmin /usr/ports/sysutils/webmin
 chown -R root:wheel /usr/ports
