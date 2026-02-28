875f59ceacbc918a1405b1d72e34acc13126a272
diamondpete <87245367+diamondpete@users.noreply.github.com>
2026-02-27 21:41:35 -0500

    replace.sh: Update and Sort

--- replace.sh
+++ replace.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-rm -rf /usr/ports/devel/py-cheetah3
-rm -rf /usr/ports/sysutils/webmin
 rm -rf /usr/ports/ftp/curl
-cp -Rp devel/py-cheetah3 /usr/ports/devel/py-cheetah3
-cp -Rp sysutils/webmin /usr/ports/sysutils/webmin
+rm -rf /usr/ports/net-p2p/jackett
+rm -rf /usr/ports/sysutils/webmin
+rm -rf /usr/ports/textproc/py-sphinx
 cp -Rp ftp/curl /usr/ports/ftp/curl
+cp -Rp net-p2p/jackett /usr/ports/net-p2p/jackett
+cp -Rp sysutils/webmin /usr/ports/sysutils/webmin
+cp -Rp textproc/py-sphinx /usr/ports/textproc/py-sphinx
 chown -R root:wheel /usr/ports
